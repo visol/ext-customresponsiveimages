3e00d132a6bd94dcc20e907dfef1a7a56fdfcf42
lorenzulrich <lulrich@hispeed.ch>
2017-09-20 10:10:33 +0000

    [TASK] Update dependencies for TYPO3 8.7, bump version to 2.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,10 +21,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '1.0.5',
+	'version' => '2.0.0-dev',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-6.2.99',
+			'typo3' => '8.7.0-8.7.99',
 		),
 		'conflicts' => array(
 		),
